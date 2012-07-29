350eca102bcbb32f079b3de00cb1ca5f064d0ea3
sportsstar89 <sjpirri@gmail.com>
2012-07-29 18:49:42 -0400

    fix scripts

--- build_cm9.sh
+++ build_cm9.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+echo going to directory
 cd ~/ics
+echo Syncing.....
 repo sync -j16
+echo Cleaning.....
 .  build/envsetup.sh
 lunch cm_ruby
 make clobber
+echo Building......
 .  build/envsetup.sh
+rm build.txt
 brunch ruby -j16 > build.txt
--- sync.sh
+++ sync.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+echo Syncing CM9
 cd ~/ics
 repo sync -j16
+echo Syncing AOKP
 cd ~/AOKP
 repo sync -j16
+echo Syncing CM10
 cd ~/jb
 repo sync -j16
