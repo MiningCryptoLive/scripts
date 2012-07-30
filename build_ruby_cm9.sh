5c73bfd127fcbf40720ca5039c59e89ee4532166
sportsstar89 <sjpirri@gmail.com>
2012-07-29 20:12:39 -0400

    new method

--- build_ruby_cm9.sh
+++ build_ruby_cm9.sh
@@ -3,11 +3,9 @@ echo going to directory
 cd ~/ics
 echo Syncing.....
 repo sync -j16
-echo Cleaning.....
-.  build/envsetup.sh
-lunch cm_ruby
-make clobber
 echo Building......
 .  build/envsetup.sh
 rm build.txt
 brunch ruby -j16 > build.txt
+cp ~/ics/out/target/product/ruby/*.zip ~/Desktop
+make clobber
